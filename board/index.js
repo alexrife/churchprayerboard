6375bf303245b68c1e12d35b10f6a2b9f389a5b6
= <bakerzach112@gmail.com>
2020-03-28 00:28:56 -0400

    Update

--- board/index.js
+++ board/index.js
@@ -1,22 +1,56 @@
+var latestObj = {};
 function load() {
     request();
 }
 
 function process(obj) {
+    latestObj = obj;
+    var html = "";
+    for (var i in obj.prayer) {
+        html = html.concat("<div class=\"prayercontainer\"><span class=\"prayer\">" + obj.prayer[i] + "</span></div>");
+        html = html.concat("<span id=\"n" + String(i) + "\" class=\"name\">By " + obj.name[i] + " - " + obj.likes[i] + " people prayed for this</span><br>");
+        html = html.concat("<button id=\"b" + String(i) + "\" class=\"pray\" onclick=\"like(" + String(i) + ");\">I prayed for this</button><hr>");
+    }
+    document.getElementById("prayers").innerHTML = html;
     console.log(obj);
 }
 
 function request() {
     var request = new XMLHttpRequest();
-    request.open("GET", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec");
+    request.open("GET", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec?cmd=get");
     request.timeout = 30000;
     request.ontimeout = function() {
         document.getElementById("wrapper").innerHTML = "<p>Error connecting to server</p>";
     };
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
-            process(JSON.parse(request.responseText));
+            var obj = JSON.parse(request.responseText);
+            obj.name.reverse();
+            obj.prayer.reverse();
+            obj.likes.reverse();
+            process(obj);
         }
     };
     request.send();
+}
+
+function like(index) {
+    document.getElementById("b" + String(index)).onclick = null;
+    document.getElementById("n" + String(index)).innerHTML = "By " + latestObj.name[index] + " - " + String(Number(latestObj.likes[index]) + 1) + " people prayed for this";
+    index = (latestObj.prayer.length - 1) - index;
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec?cmd=like");
+    request.timeout = 30000;
+    request.ontimeout = function() {
+        document.getElementById("wrapper").innerHTML = "<p>Error connecting to server</p>";
+    };
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            console.log(JSON.parse(request.responseText));
+        }
+    };
+    console.log(index);
+    request.send(JSON.stringify({
+        index: index
+    }));
 }
--- submissions/index.js
+++ submissions/index.js
@@ -3,20 +3,39 @@ function load() {
 }
 
 function process(obj) {
-    console.log(obj);
+    document.getElementById("error").innerHTML = "<p>Sent!</p><hr>";
+    if (obj.result == "error") {
+        document.getElementById("error").innerHTML = "<p>An Error has occured: " + obj.error.message + "</p><hr>";
+    }
 }
 
 function request(name, prayer) {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec");
+    request.open("POST", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec?cmd=new");
     request.timeout = 30000;
     request.ontimeout = function() {
-        document.getElementById("").innerHTML = "<p>Couldn't Connect to Server</p><hr>";
+        document.getElementById("error").innerHTML = "<p>Couldn't Connect to Server</p><hr>";
     };
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
             process(JSON.parse(this.responseText));
+            document.getElementById("submit").onclick = submitHandler;
         }
     };
-    request.send();
+    request.send(JSON.stringify({
+        name:name,
+        prayer:prayer
+    }));
+};
+function submitHandler() {
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("submit").onclick = function(){};
+    var name = document.getElementById("name").value;
+    var prayer = document.getElementById("prayer").value;
+    if (!(/^\w.*\w$/.test(name)) && !(/^\w$/.test(name)) && name != "") return;
+    if (!(/^.+$/.test(prayer))) return;
+    if (name == "" || name == null) name = "Anonymous";
+    request(name, prayer);
+    document.getElementById("name").value = "";
+    document.getElementById("prayer").value = "";
 }
