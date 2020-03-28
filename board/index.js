711b92bdebf2ba3f1b8051aa5a5f0dffdb423f0c
= <bakerzach112@gmail.com>
2020-03-27 22:24:24 -0400

    Update

--- board/index.js
+++ board/index.js
@@ -1,3 +1,22 @@
 function load() {
-    
+    request();
+}
+
+function process(obj) {
+    console.log(obj);
+}
+
+function request() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec");
+    request.timeout = 30000;
+    request.ontimeout = function() {
+        document.getElementById("wrapper").innerHTML = "<p>Error connecting to server</p>";
+    };
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            process(JSON.parse(request.responseText));
+        }
+    };
+    request.send();
 }
--- submissions/index.js
+++ submissions/index.js
@@ -1,3 +1,22 @@
 function load() {
     
 }
+
+function process(obj) {
+    console.log(obj);
+}
+
+function request(name, prayer) {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://script.google.com/macros/s/AKfycbxlnSbYs0jokS6KXF_OPSvcsngIxHObD6Iowy_N0ow9qfwTDDY/exec");
+    request.timeout = 30000;
+    request.ontimeout = function() {
+        document.getElementById("").innerHTML = "<p>Couldn't Connect to Server</p><hr>";
+    };
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            process(JSON.parse(this.responseText));
+        }
+    };
+    request.send();
+}
